ab071ca8ffe077fe0e9d11eb387efc5f865dda05
jensen <jensen.jones111@gmail.com>
2025-06-25 16:45:12 +1000

    BB data produce

---
+++ greeks/BollingerBandsSingleDirection.py
@@ -0,0 +1,36 @@
+import numpy as np
+
+from greeks.BollingerBandsCalculator import BollingerBandsCalculator
+from greeks.GreekBaseClass import Greek
+
+
+class BollingerBandsSingleDirection(Greek):
+    def __init__(self, prices, bbc: BollingerBandsCalculator, focusBand: str, comparator):
+        super().__init__()
+        self.recentDayPrices = prices[:, -1]
+        self.bbc = bbc
+        self.focusBand = focusBand
+        self.comparator = comparator
+
+        self.signal = np.full(bbc.pricesSoFar.shape[0], np.nan)
+
+        if bbc.getSma() is not None:
+            self.calculateSignal()
+
+    def update(self, newDayPrices: np.ndarray):
+        self.bbc.update(newDayPrices)
+
+        if self.bbc.getSma() is not None:
+            self.calculateSignal()
+
+    def getGreeks(self):
+        return self.signal
+
+    def calculateSignal(self):
+        band = None
+        if self.focusBand == "upper":
+            band = self.bbc.getUpperBands()
+        else:
+            band = self.bbc.getLowerBands()
+
+        self.signal = self.comparator(self.recentDayPrices, band)
--- greeks/ProduceGreeksData.py
+++ greeks/ProduceGreeksData.py
@@ -1,5 +1,9 @@
 import sys
 import os
+
+from greeks.BollingerBandsCalculator import BollingerBandsCalculator
+from greeks.BollingerBandsSingleDirection import BollingerBandsSingleDirection
+
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 
 from greeks.GreeksManager import GreeksManager
@@ -18,6 +22,7 @@ ROLLING_MEANS_WINDOW_SIZE = 14
 RSIC_WINDOW_SIZE = 14
 RSI_LONG_THRESHOLD = 30
 RSI_SHORT_THRESHOLD = 70
+BB_WINDOW_SIZE = 20
 
 prices = np.loadtxt("./sourceCode/prices.txt").T
 
@@ -26,11 +31,23 @@ def main():
     longRsiC = RsiCalculator(pricesSoFar, RSIC_WINDOW_SIZE)
     shortRsiC = RsiCalculator(pricesSoFar, RSIC_WINDOW_SIZE)
 
+    lowerBbc = BollingerBandsCalculator(pricesSoFar, BB_WINDOW_SIZE)
+    upperBbc = BollingerBandsCalculator(pricesSoFar, BB_WINDOW_SIZE)
+
+    def lowerBbComp(a: np.ndarray, b: np.ndarray) -> np.ndarray:
+        return (a < b).astype(int)
+    def upperBbComp(a: np.ndarray, b: np.ndarray) -> np.ndarray:
+        return (a > b).astype(int)
+
     lagged_prices_greeks = [LaggedPrices(pricesSoFar, lag) for lag in range(LAG_START_RANGE, LAG_END_RANGE)]
-    greeks = lagged_prices_greeks + [LogReturns(pricesSoFar),
+    greeks = lagged_prices_greeks + [
+                                     LogReturns(pricesSoFar),
                                      RollingMeans(pricesSoFar, ROLLING_MEANS_WINDOW_SIZE),
                                      RsiSingleDirection(longRsiC, "long", RSI_LONG_THRESHOLD),
-                                     RsiSingleDirection(shortRsiC, "short", RSI_SHORT_THRESHOLD)]
+                                     RsiSingleDirection(shortRsiC, "short", RSI_SHORT_THRESHOLD),
+                                     BollingerBandsSingleDirection(pricesSoFar, lowerBbc, "lower", lowerBbComp),
+                                     BollingerBandsSingleDirection(pricesSoFar, upperBbc, "upper", upperBbComp)
+                                     ]
     gm = GreeksManager(greeks)
 
     produceGreeksData(gm)
@@ -51,12 +68,17 @@ def produceGreeksData(gm):
             if hasattr(greek, 'lag'):
                 greek_name += f"_Lag={greek.lag}"
 
+            if hasattr(greek, 'focusBand'):
+                greek_name += f"_focusBand={greek.focusBand}"
+
             if greek_name not in toLog:
                 toLog[greek_name] = []
 
+            assert(greek.getGreeks().shape[0] == 50)
             toLog[greek_name].append(greek.getGreeks())
 
     for greekToLog, listOfGreeks in toLog.items():
+        print(f"Appending greeks of name {greekToLog}\nShape: {np.stack(listOfGreeks).shape}\n")
         np.save(f"./greeks/greeksData/{greekToLog}_750_day_data.npy", np.stack(listOfGreeks))
 
 
